b4ca3de83c2f9868bc721866959b263adb2041ba
MahimPC111 <mahimpc111@gmail.com>
2022-11-28 21:58:07 +0600

    final commit

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
-const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 app.use(cors())
 app.use(express.json());
